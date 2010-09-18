767db45b9de5120521813bd7bf3c135164a37f4f
Andrew R <andrewreds@gmail.com>
2010-09-18 17:12:23 +1000

    fixed geoip to move map and show address
    Signed-off-by: Andrew R <andrewreds@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+var geocoder;
+var map;
 
 function load () {
+	geocoder = new google.maps.Geocoder();
 	var latlng = new google.maps.LatLng(-34.397, 150.644);
 	var myOptions = {
 		zoom: 8,
 		center: latlng,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-	var map = new google.maps.Map(document.getElementById("map"), myOptions);
+	map = new google.maps.Map(document.getElementById("map"), myOptions);
 }
 
 
@@ -21,15 +24,33 @@ function validate (type, obj) {
 	}
 }
 
+function show(lat,lng) {
+	var latlng = new google.maps.LatLng(lat, lng);
+	map.setCenter(latlng);
+	geocoder.geocode({'latLng': latlng}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			if (results[0]) {
+				map.setZoom(13);
+				document.getElementById("from").value=results[0].formatted_address;
+			}
+		} else {
+			alert("Geocoder failed due to: " + status);
+		}
+	});
+}
+
 function successLocation (location) {
 	obj=document.getElementById("from");
+	
 	obj.value=location.coords.latitude+" "+location.coords.longitude;
+	show (location.coords.latitude,location.coords.longitude);
 	validate ("location", obj);
 }
 
 function geoIP () {
 	obj=document.getElementById("from");
 	obj.value=geoip_city()+", "+geoip_region_name()+", "+geoip_country_name();
+	show (geoip_latitude(),geoip_longitude());
 	validate ("location", obj);
 }
 
@@ -37,6 +58,9 @@ function showLocation () {
 	geoIP();
 	if (navigator.geolocation) {
 	  navigator.geolocation.getCurrentPosition(successLocation);
+	} else if (google.gears) {
+		var geo = google.gears.factory.create('beta.geolocation');
+		geo.getCurrentPosition(successLocation);
 	}
 	
 	return false;

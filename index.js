17a04af73fdbe473d159099724270c384022df16
Andrew R <andrewreds@gmail.com>
2010-09-18 17:59:00 +1000

    actualy got the address valadation to work
    Signed-off-by: Andrew R <andrewreds@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var geocoder;
 var map;
+var time=null;
 
 function load () {
 	geocoder = new google.maps.Geocoder();
@@ -10,28 +11,46 @@ function load () {
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	map = new google.maps.Map(document.getElementById("map"), myOptions);
+	
+	geoIP();
 }
 
-
+function checkAddress (obj) {
+var address = obj.value;
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			$("#"+obj.id).removeClass("invalid");
+			$("#"+obj.id).addClass("valid");
+		} else {
+			$("#"+obj.id).removeClass("valid");
+			$("#"+obj.id).addClass("invalid");
+		}
+	});
+}
 
 function validate (type, obj) {
 	if (obj.value == "") {
 		$("#"+obj.id).removeClass("valid");
 		$("#"+obj.id).addClass("invalid");
 	} else {
-		$("#"+obj.id).addClass("valid");
-		$("#"+obj.id).removeClass("invalid");
+		if (time!=null) {
+			clearTimeout (time);
+		}
+		time = setTimeout (function(){
+			checkAddress(obj);
+			time=null;
+		},500);
 	}
 }
 
-function show(lat,lng) {
+function show(lat,lng,level) {
 	var latlng = new google.maps.LatLng(lat, lng);
 	map.setCenter(latlng);
 	geocoder.geocode({'latLng': latlng}, function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK) {
-			if (results[0]) {
+			if (results[level]) {
 				map.setZoom(13);
-				document.getElementById("from").value=results[0].formatted_address;
+				document.getElementById("from").value=results[level].formatted_address;
 			}
 		} else {
 			alert("Geocoder failed due to: " + status);
@@ -43,19 +62,18 @@ function successLocation (location) {
 	obj=document.getElementById("from");
 	
 	obj.value=location.coords.latitude+" "+location.coords.longitude;
-	show (location.coords.latitude,location.coords.longitude);
+	show (location.coords.latitude,location.coords.longitude,0);
 	validate ("location", obj);
 }
 
 function geoIP () {
 	obj=document.getElementById("from");
 	obj.value=geoip_city()+", "+geoip_region_name()+", "+geoip_country_name();
-	show (geoip_latitude(),geoip_longitude());
+	show (geoip_latitude(),geoip_longitude(),2);
 	validate ("location", obj);
 }
 
 function showLocation () {
-	geoIP();
 	if (navigator.geolocation) {
 	  navigator.geolocation.getCurrentPosition(successLocation);
 	} else if (google.gears) {

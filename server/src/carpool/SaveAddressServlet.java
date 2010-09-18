3bd0a3b53c82e7d686e9e8faca1d76a35d969bff
Nathan Sturgess <nathansturgess.uni@gmail.com>
2010-09-18 16:42:08 +1000

    Linked the user input with the carPoolReq object

--- server/src/carpool/SaveAddressServlet.java
+++ server/src/carpool/SaveAddressServlet.java
@@ -1,6 +1,8 @@
 package carpool;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -35,11 +37,45 @@ public class SaveAddressServlet extends HttpServlet {
 		
 		AddressDao dao = new AddressDao();
 		dao.saveAddress(carpoolReq);
+		
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet</title>");  
+            out.println("</head>");
+            out.println("<body>");
+
+            String from = request.getParameter("from");
+            String to = request.getParameter("to");
+            String country = request.getParameter("country");
+            String contactName = request.getParameter("contactName");
+            String email = request.getParameter("email");
+            String phone = request.getParameter("phone");
+
+            out.println("<h1> You are going from: " + from + "</h1>");
+            out.println("<h1> You are going to: " + to + "</h1>");
+            out.println("<h1> Your country is: " + country + "</h1>");
+            out.println("<h1> Contact name is: " + contactName + "</h1>");
+            out.println("<h1> Your email is: " + email + "</h1>");
+            out.println("<h1> Your phone number is: " + phone + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+         
+        } finally { 
+            out.close();
+        }
 	}
 	
 	private CarpoolRequest getCarpoolRequest(HttpServletRequest request){
 		CarpoolRequest carpoolReq = new CarpoolRequest();
-		
+		carpoolReq.setContactName(request.getParameter("contactName"));
+		carpoolReq.setCountry(request.getParameter("country"));
+		carpoolReq.setEmail(request.getParameter("email"));
+		carpoolReq.setFrom(request.getParameter("from"));
+		carpoolReq.setPhone(request.getParameter("phone"));
+		carpoolReq.setTo(request.getParameter("to"));
 		return carpoolReq;
 	}
 
